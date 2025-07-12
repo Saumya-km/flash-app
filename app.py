76c6e7b25cb9d027b7fed3d8babcd467413c3eeb
Saumya-km <saumyasingh9653@gmail.com>
2025-07-12 22:47:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from flask import Flask, render_template, request
-
+from flask import Flask
 app = Flask(__name__)
 
-flashcards = [
-    {"question": "What is the capital of France?", "answer": "Paris"},
-    {"question": "What is 2 + 2?", "answer": "4"},
-    {"question": "What is the color of the sky?", "answer": "Blue"},
-]
-
 @app.route('/')
-def index():
-    card_id = int(request.args.get("card", 0))
-    card = flashcards[card_id % len(flashcards)]
-    return render_template("index.html", card=card, card_id=card_id)
+def hello_word():
+       return 'Hello Docker, welcome to DevOps world'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/health')
+def health():
+    return 'Server is up and running'
